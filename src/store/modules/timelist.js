172e88895bdfb09b881ba75da41e808c5d1417fa
Wang Ze <1044891738@qq.com>
2019-11-08 00:31:59 +0800

    提交

---
+++ src/store/modules/timelist.js
@@ -0,0 +1,25 @@
+import {showDynamic} from '../../servier/index'
+
+const state={
+    list:[]
+}
+
+const mutations={
+    updateList(state,payload){
+        state.list=payload
+    }
+}
+
+const actions={
+    async getTimeline(context,payload){
+        let data=await showDynamic();
+        context.commit('updateList',data.data.data);
+    }
+}
+
+export default{
+    namespaced:true,
+    state,
+    mutations,
+    actions
+}
